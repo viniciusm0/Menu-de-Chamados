6f71e5d7c964b148a9c047ea5b1426664eb51236
Vinicius <viniciusm1603@gmail.com>
2024-04-23 08:53:45 -0300

    novos botoes e animações

--- assets/script.js
+++ assets/script.js
@@ -1,20 +1,35 @@
+var searchbar = document.getElementById("searchbar");
+var menucriar = document.getElementById("menucriacao");
+
 function searchbarON() {
+    searchbar.style.animationName = "searchbar-animation"
+
     document.querySelector('#searchbar').classList.remove('search-bar-off');
     document.querySelector('#searchbar').classList.add('search-bar-on');
 }
 
 function searchbarOFF() {
-    document.querySelector('#searchbar').classList.remove('search-bar-on');
-    document.querySelector('#searchbar').classList.add('search-bar-off');
+    searchbar.style.animationName = "searchbar-out";
+
+    setTimeout (function(){
+        document.querySelector('#searchbar').classList.remove('search-bar-on');
+        document.querySelector('#searchbar').classList.add('search-bar-off');
+    }, 801)
 }
 
 function menucriarON() {
+    menucriar.style.animationName = "newinfo-animation";
     document.querySelector('#menucriacao').classList.remove('menu-criacao-off')
     document.querySelector('#menucriacao').classList.add('menu-criacao-on')
 }
 
 function menucriarOFF() {
-    document.querySelector('#menucriacao').classList.remove('menu-criacao-on')
-    document.querySelector('#menucriacao').classList.add('menu-criacao-off')
+    menucriar.style.animationName = "newinfo-out";
+
+    setTimeout (function(){
+        document.querySelector('#menucriacao').classList.remove('menu-criacao-on')
+        document.querySelector('#menucriacao').classList.add('menu-criacao-off')
+    }, 1001)
+    
 
 }
